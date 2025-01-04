0eabf7b00643f3a25661afd1346351b5a52ff310
jsy5888 <oliviasunjin0318@gmail.com>
2025-01-03 20:21:36 -0500

    favorite movie

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,48 +1,123 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { Router } from '@angular/router'
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { error } from 'console';
+import { response } from 'express';
 
 
 @Component({
   selector: 'app-movie-card',
   standalone: false,
-
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
-  movies: any[] = [];
+
+export class MovieCardComponent implements OnInit {
+  movies: any[] = []; //movie lists from server
+  filteredMovies: any[] = []; // Filtered List of movies 
+  selectedGenre: string = ''; // Selected genre
+  favoriteMovies: string[] = [];
+  userName: string = '';
+
   constructor(
     public fetchApiData: FetchApiDataService,
-    private router: Router
+    private router: Router,
   ) { }
-
-  goToUserProfile(): void {
-    this.router.navigate(['/user-profile']);
-  }
 
   ngOnInit(): void {
     this.getMovies();
+    // this.getFavoriteMovie();
   }
 
   // used to fetch the movies from FetchApiDataService 
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
-      console.log(this.movies);
+      this.filteredMovies = [...this.movies];
       return this.movies;
     });
   }
 
-  // // Navigate to the Welcome view
-  // goToWelcome() {
-  //   this.router.navigate(['/']);
+  // Go to User Profile View
+  goToUserProfile(): void {
+    this.router.navigate(['/user-profile']);
+  }
+
+
+  // Show Info
+  toggleInfo(movie: any, type: string): void {
+
+    // show only clicked one
+    if (type === 'genre') {
+      movie.showGenre = !movie.showGenre;
+      movie.showDirector = false;
+      movie.showSynopsis = false;
+    } else if (type === 'director') {
+      movie.showDirector = !movie.showDirector;
+      movie.showGenre = false;
+      movie.showSynopsis = false;
+    } else if (type === 'synopsis') {
+      movie.showSynopsis = !movie.showSynopsis;
+      movie.showGenre = false;
+      movie.showDirector = false;
+    }
+  }
+
+
+  toggleFavorite(movie: any): void {
+    if (this.isFavorite(movie._id)) {
+
+      this.deleteFavorite(movie._id);
+    }
+    else {
+      this.addFavorite(movie._id);
+    }
+  }
+
+
+  // // Get favorite movies
+  // getFavoriteMovie(): void {
+  //   this.fetchApiData.getFavoriteMovie(this.userName).subscribe((resp: any) => {
+  //     (
+  //       this.favoriteMovies = resp.favoriteMovies); // Store only IDs
+  //   },
+  //     (error) => {
+  //       console.error('Error fetching favorite movies:', error);
+  //     }
+  //   );
   // }
 
-  // // Navigate to movie page
-  // goToProfile(username:string){
-  //   this.router.navigate(['/user-profile' ]);
-  // }
-  
+  // Add favorite movies
+  addFavorite(movie: any): void {
+    this.fetchApiData.addFavoriteMovie(movie._id).subscribe(
+      (resp: any) => {
+        // Add the newly added movie to the favoriteMovies array
+        this.favoriteMovies.push(movie._id);
+        console.log('Movie added to favorites:', resp);
+      },
+      (error: any) => {
+        console.error('Error adding movie to favorites:', error);
+      }
+    );
+  }
+
+  // remove favorite movies
+  deleteFavorite(movie: any): void {
+    this.fetchApiData.deleteFavoriteMovie(movie._id).subscribe(
+      (resp: any) => {
+        this.favoriteMovies = this.favoriteMovies.filter(id => id !== movie._id);
+
+      },
+      (error: any) => {
+        console.error('Error removing movie from favorites:', error);
+      }
+    );
+  }
+
+
+  isFavorite(movieId: string): boolean {
+    return this.favoriteMovies.includes(movieId);
+  }
 
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -21,19 +21,22 @@ export class UserProfileComponent implements OnInit {
     Birthday: ''
   };
   updatedUserData: any = {}; //hold the updated user data for editing
+  favoriteMovies: any[] = []; // Declare the favoriteMovies array here
+  movies: any[] = []; // Declare movies array to store all movies
 
   constructor(
     private fetchApiData: FetchApiDataService,
     private router: Router) { }
 
+
   ngOnInit(): void {
-    const username = localStorage.getItem('username');
+    const username = localStorage.getItem('user');
 
     if (username) {
       this.fetchApiData.getUser(username).subscribe(
         (data) => {
-          this.userData = data;
-          this.updatedUserData = { ...data };
+          this.userData = data.find((u: any) => u.Username === username);
+          this.updatedUserData = { ...this.userData };
         },
         (error) => {
           console.error('Error fetching user data', error)
@@ -42,11 +45,23 @@ export class UserProfileComponent implements OnInit {
     } else {
       console.error('User is not logged in!');
     }
-  }
 
+    // Fetch all movies to display in favorites
+    this.fetchApiData.getAllMovies().subscribe(
+      (movies) => {
+        this.movies = movies;
+        console.log('Movies fetched:', this.movies);
+        this.loadFavoriteMovies(); // Load favorite movies after movies are fetched
+      },
+      (error) => {
+        console.error('Error fetching movies', error);
+      }
+    );
+  }
+  
   // Update user profile
   updateUserProfile(): void {
-    const username = localStorage.getItem('username');
+    const username = localStorage.getItem('user');
 
     if (username) {
       this.fetchApiData.editUser(username, this.updatedUserData).subscribe(
@@ -60,4 +75,52 @@ export class UserProfileComponent implements OnInit {
       console.error('User is not logged in!')
     }
   }
-}
+
+  // Navigate to Movie Card
+  goToMovieCard(): void {
+    this.router.navigate(['/movie-card']);
+  }
+
+  // Navigate to Welcome Page (Log Out)
+  goToWelcomePage(): void {
+    this.router.navigate(['/welcome-page']);
+  }
+
+  // Log Out 
+  logout(): void {
+    localStorage.removeItem('authToken');
+    this.router.navigate(['/welcome-page']);
+  }
+
+  // 로그인 상태 확인 (토큰이 있으면 로그인 상태로 간주)
+  isAuthenticated(): boolean {
+    return localStorage.getItem('authToken') !== null;
+  }
+
+  // Load favorite movies from localStorage
+  loadFavoriteMovies(): void {
+    const storedFavorites = localStorage.getItem('isFavorite');
+    if (storedFavorites) {
+      try {
+        this.favoriteMovies = JSON.parse(storedFavorites);
+        console.log('Favorite Movies loaded from localStorage:', this.favoriteMovies);
+      } catch (error) {
+        console.error('Error parsing favorite movies from localStorage:', error);
+      }
+    } else {
+      console.log('No favorite movies found in localStorage');
+    }
+  }
+
+    // Get details of favorite movies
+    getFavoriteMoviesDetails() {
+      console.log('Getting favorite movie details', this.favoriteMovies);
+      return this.favoriteMovies.map((movieId: number) => {
+        const movie = this.movies.find(m => m.id === movieId || m.id === Number(movieId));
+        if (!movie) {
+          console.warn(`Movie with ID ${movieId} not found`);
+        }
+        return movie; // This should return full movie details
+      }).filter(movie => movie !== undefined); // Remove any undefined values
+    }
+  }
